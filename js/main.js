01b20c6b80f0e9579a8dd937a0ec4df35b809a38
Joshua Baker <jbaker264@yahoo.com>
2022-07-20 22:48:02 +0000

    Revised Issue 2, removed class change to hidden from the loop. Removed load event listener and just called swapViews function in domcontentloaded event handler

--- js/main.js
+++ js/main.js
@@ -64,10 +64,11 @@ function renderEntry(entry) {
 }
 
 function handleDomContent(event) {
+  $noEntriesText.className = 'hidden';
+  swapViews(event);
   for (var i = 0; i < data.entries.length; i++) {
     var newEntry = renderEntry(data.entries[i]);
     $ul.append(newEntry);
-    $noEntriesText.className = 'hidden';
   }
 }
 window.addEventListener('DOMContentLoaded', handleDomContent);
