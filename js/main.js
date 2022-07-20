e530cd3b2fecb5aae54a082bc9a6f3423f22b5fc
Joshua Baker <jbaker264@yahoo.com>
2022-07-20 20:23:40 +0000

    Ensure that submitting a new journal entry also creates a new DOM tree for it and adds it to the page.Ensure that submitting a new journal entry automatically shows the 'entries' view without reloading the page.Ensure that refreshing the pages shows the same view as before refreshing.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ var $image = document.querySelector('#entry-img');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
 var $formInputs = document.getElementById('inputs');
+var $ul = document.querySelector('ul');
 
 function updateImgSrc(event) {
   $image.setAttribute('src', event.target.value);
 }
 $photoUrl.addEventListener('input', updateImgSrc);
 
-function saveEntry(event) {
+function handleSubmit(event) {
   event.preventDefault();
   var entryData = {
     title: $title.value,
@@ -21,9 +22,13 @@ function saveEntry(event) {
   data.entries.unshift(entryData);
   $image.setAttribute('src', '/images/placeholder-image-square.jpg');
   $formInputs.reset();
+  data.view = 'entries';
+  $ul.prepend(renderEntry(entryData));
+  swapViews();
+  $formInputs.reset();
 }
 
-$formInputs.addEventListener('submit', saveEntry);
+$formInputs.addEventListener('submit', handleSubmit);
 
 function renderEntry(entry) {
   var $listItem = document.createElement('li');
@@ -49,15 +54,49 @@ function renderEntry(entry) {
   $entryH2.textContent = entry.title;
   $colHalfEntry.appendChild($entryH2);
 
+  var $entryP = document.createElement('p');
+  $entryP.textContent = entry.notes;
+  $colHalfEntry.appendChild($entryP);
+
   return $listItem;
 }
 
-var $ul = document.querySelector('ul');
+var $noEntriesText = document.querySelector('.no-entries-text');
 
 function handleDomContent(event) {
   for (var i = 0; i < data.entries.length; i++) {
     var newEntry = renderEntry(data.entries[i]);
     $ul.append(newEntry);
+    $noEntriesText.className = 'hidden';
   }
 }
 window.addEventListener('DOMContentLoaded', handleDomContent);
+
+var $headerEntries = document.querySelector('.header-entries');
+var $newButton = document.getElementById('new-btn');
+var $entryForm = document.querySelector('.view');
+var $entries = document.querySelector('.entries');
+
+function swapViews() {
+  if (data.view === 'entries') {
+    $entries.className = 'entries';
+    $entryForm.className = 'hidden';
+  } else if (data.view === 'entry-form') {
+    $entries.className = 'hidden';
+    $entryForm.className = 'entry-form';
+  }
+}
+
+function swapViewToEntries(event) {
+  data.view = 'entries';
+  swapViews();
+}
+$headerEntries.addEventListener('click', swapViewToEntries);
+
+function swapViewToForm(event) {
+  data.view = 'entry-form';
+  swapViews();
+}
+$newButton.addEventListener('click', swapViewToForm);
+
+window.addEventListener('load', swapViews(event));
